2bbd31621d68dd72b134551055cdbfebe8b1272d
sinemsooplay <96748658+sinemkadakall@users.noreply.github.com>
2024-12-03 18:09:52 +0300

    Grounding completed.

--- DonkeyGame/Assets/Scripts/Player.cs
+++ DonkeyGame/Assets/Scripts/Player.cs
@@ -6,19 +6,47 @@ public class Player : MonoBehaviour
 {
 
     private new Rigidbody2D rigidbody;
+    private Collider2D collider;
+    private Collider2D[] results;
     private Vector2 direction;
     public float speed=1f;
     public float jumpSpeed=1f;
 
+    private bool grounded;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        results = new Collider2D[4];
     }
+    private void CheckCollision()
+    {
+        grounded = false;
 
+        Vector2 size=collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+        int amaount = Physics2D.OverlapBoxNonAlloc(transform.position,size,0f,results);
+
+        for (int i = 0; i < amaount; i++)
+        {
+            GameObject hit = results[i].gameObject;
+
+            if(hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                grounded = hit.transform.position.y < (transform.position.y - 0.5f);
+
+                Physics2D.IgnoreCollision(collider, results[i],!grounded);
+            }
+        }
+
+    }
     private void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        CheckCollision();
+
+        if (grounded && Input.GetButtonDown("Jump"))
         {
             direction = Vector2.up * jumpSpeed;
         }
@@ -27,9 +55,14 @@ public class Player : MonoBehaviour
             direction += Physics2D.gravity * Time.deltaTime;
         }
         direction.x = Input.GetAxis("Horizontal")*speed;
-        direction.y = Mathf.Max(direction.y, -1f);
 
-        if(direction.x > 0)
+        if (grounded)
+        {
+            direction.y = Mathf.Max(direction.y, -1f);
+
+        }
+
+        if (direction.x > 0)
         {
             transform.eulerAngles = Vector3.zero;
         }
